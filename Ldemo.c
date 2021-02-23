63fb76b0720e510a829a0fc9642381ca12950c3b
AjayMuthyaboyina <79127695+AjayMuthyaboyina@users.noreply.github.com>
2021-02-23 01:37:40 -0800

    Create Ldemo.c

---
+++ Ldemo.c
@@ -0,0 +1,16 @@
+#include<stdioh.>
+#include<sys/types.h>
+#include<unistd.h>
+int main()
+{
+int pid=0;
+pid=fork();
+if(pid==0)
+{
+printf("In child");
+}
+else
+{
+printf("In Parent process id of child is %d");
+return 0;
+}
